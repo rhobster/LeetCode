f1a42f79a279ddd0f32b242fd0156654477a68c7
rhobster <65625385+rhobster@users.noreply.github.com>
2022-09-11 19:49:46 -0400

    Add Sum better solution

--- Sum.cs
+++ Sum.cs
@@ -1,5 +1,6 @@
 public class Solution {
     public int[] TwoSum(int[] nums, int target) {
+        /* My Original Solution, O(n^2)
         int[] indices = new int[]{-1, -1};
         int baseIndex = 0;
         while(baseIndex < nums.Length - 1){
@@ -14,5 +15,19 @@ public class Solution {
         } 
         //it should never get here. but this would be n^2 because it goes through nums twice.
         return indices;
+        */
+
+        //Better Solution, O(n)
+        Dictionary<int,int> dic = new Dictionary<int,int>();
+    
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(dic.ContainsKey(target - nums[i]))
+                return new int[]{i, dic[target - nums[i]]};
+            else if(!dic.ContainsKey(nums[i]))
+                dic.Add(nums[i], i);
+        }
+        
+        return new int[]{-1, -1};;
     }
 }
